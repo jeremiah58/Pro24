372d1aac52dc968b31c0f4fc84868d91bf526aad
jeremiah58 <100638480+jeremiah58@users.noreply.github.com>
2022-03-23 17:02:31 -0400

    Add files via upload

---
+++ Iron.js
@@ -0,0 +1,8 @@
+class Iron{
+    constructor(x,y)
+}
+var options = {
+   'restitution':0.3,
+   'friction':3,
+   'density':30 
+}
---
+++ Stone.js
@@ -0,0 +1,8 @@
+class Stone{
+    constructor(x,y,width,height)
+}
+var options = {
+    'restitution':0.8,
+    'friction':0.9,
+    'density':12
+}
